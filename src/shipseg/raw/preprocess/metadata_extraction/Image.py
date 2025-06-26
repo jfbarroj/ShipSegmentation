a0a90fb998899f2c72109cd0b56a62506af266e4
José Félix Barba Rojas <josefelix.barba@ubotica.com>
2025-06-26 15:36:12 +0200

    Fixed Image.py and Mask.py

--- src/shipseg/raw/preprocess/metadata_extraction/Image.py
+++ src/shipseg/raw/preprocess/metadata_extraction/Image.py
@@ -3,43 +3,43 @@ import numpy.typing as npt
 import pathlib
 import cv2
 from dataclasses import dataclass
-from typing import Self
+from typing import Self, List
 
 @dataclass
 class Image:
     data: npt.NDArray[np.uint8] #RGB planar format
     path: pathlib.Path
 
-def __init__(self: Image, path: pathlib.Path) -> Image:
-    self.path = path
-    array = cv2.imread(path)
-    array = np.array(array, dtype = 'uint8')
-    array = array.transpose(2,0,1)
-    array = array[::-1]
-    self.data = array
+    def __init__(self, path: pathlib.Path):
+        self.path = path
+        array = cv2.imread(path)
+        array = np.array(array, dtype = 'uint8')
+        array = array.transpose(2,0,1)
+        array = array[::-1]
+        self.data = array
 
-@classmethod
-def from_path(cls, img_path: pathlib.Path) -> Self:
-    array = cv2.imread(img_path)
-    array = np.array(array, dtype = 'uint8')
-    array = array.transpose(2,0,1)
-    array = array[::-1]
-    return cls(data = array, path = img_path)
+    @classmethod
+    def from_path(cls, img_path: pathlib.Path) -> Self:
+        array = cv2.imread(img_path)
+        array = np.array(array, dtype = 'uint8')
+        array = array.transpose(2,0,1)
+        array = array[::-1]
+        return cls(data = array, path = img_path)
 
-@property
-def getPath(self: Image) -> pathlib.Path:
-    return self.path
+    @property
+    def getPath(self) -> pathlib.Path:
+        return self.path
 
-@property
-def getImgId(self: Image) -> str:
-    return self.path.stem
+    @property
+    def getImgId(self) -> str:
+        return self.path.stem
 
-@property
-def getNbands(self: Image) -> int:
-    return self.data.shape[0]
+    @property
+    def getNbands(self) -> int:
+        return self.data.shape[0]
 
-def getMin(self: Image, channel: int) -> np.uint8:
-    return self.data[channel].min()
+    def getMin(self, channel: int) -> np.uint8:
+        return self.data[channel].min()
 
-def getMax(self: Image, channel: int) -> np.uint8:
-    return self.data[channel].max()
+    def getMax(self, channel: int) -> np.uint8:
+        return self.data[channel].max()
